6d0f0ca5f04b51b6916b3b120ddb596af8929aba
anton <belovvadum@gmail.com>
2022-03-28 22:54:33 +0300

    add Mysql

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ttBooksAndAuthors',
+    'username' => 'ttBooksAndAuthors',
+    'password' => 'Test1467',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
